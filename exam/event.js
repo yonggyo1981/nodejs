2f264ddbd1640219c4a1bbbd1a3ec1fa4ae2c63b
yonggyo1981 <yonggyo1981@gmail.com>
2021-07-23 16:10:00 +0900

    .

--- exam/event.js
+++ exam/event.js
@@ -22,9 +22,22 @@ MyEvent2.addListener("walk", () => {
 	console.log("걸어가고 있다");
 });
 
-MyEvent2.addListener("ride", () => {
+const listener = () => {
 	console.log("버스 타고 가고 있다");
-});
+};
+
+const listener2 = () => {
+	console.log("지하철을 타고 가고 있다");
+};
+
+MyEvent2.addListener("ride", listener);
+MyEvent2.addListener("ride", listener2);
+
+
+//MyEvent2.off("ride", listener);
+//MyEvent2.removeListener("ride", listener);
+MyEvent2.removeAllListeners("ride");
+
 
 setTimeout(() => {
 	MyEvent2.emit("ride");
