218c02e6d4db094de37de81b310c37998a77c28a
yonggyo1981 <yonggyo1981@gmail.com>
2021-07-30 13:22:39 +0900

    .

--- exam/express/exam6/routes/file.js
+++ exam/express/exam6/routes/file.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const multer = require('multer');
+const path = require('path');
 const router = express.Router();
 
 /** multer 설정 부분 const upload = multer({ ... 설정 ... }); */
-
+const upload = multer({
+	storage : multer.diskStorage({
+		destination(req, file, done) {
+			done(null, path.join(__dirname, "../public/upload"));
+		},
+		filename(req, file, done) {
+			/**
+			 파일명 + timestamp + 확장자 -> 중복을 방지 
+			*/
+			const ext = path.extname(file.originalname);
+			const filename = path.basename(file.originalname, ext) + "_" + Date.now() + ext;
+			done(null, filename);
+		},
+	}),
+	limits : { fileSize: 10 * 1024 * 1024 }, // 10mb
+});
 
 router.get("/", (req, res) => {
 	return res.render("file");
