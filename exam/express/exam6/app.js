1c388ad8a8117318f9cf6cb92ed499feb3ca71e7
yonggyo1981 <yonggyo1981@gmail.com>
2021-07-30 10:22:39 +0900

    .

---
+++ exam/express/exam6/app.js
@@ -0,0 +1,60 @@
+const express = require('express');  
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const logger = require('./lib/logger');
+
+/** 라우터 */
+const indexRouter = require('./routes'); // index.js 생략..
+
+const app = express();
+
+dotenv.config(); // .env -> process.env 하위 속성 추가 
+
+app.set("view engine", "html");
+nunjucks.configure(path.join(__dirname, 'views'), {
+	express : app,
+	watch : true,
+});
+
+app.set('PORT', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+/** 라우터 등록 */
+app.use(indexRouter);
+
+
+/** 없는 페이지 처리 라우터 */
+app.use((req, res, next) => {
+	const err = new Error(`${req.url}은 없는 페이지 입니다.`);
+	err.status = 404;
+	next(err); // 오류 처리 라우터로 이동
+});
+
+/** 오류처리 라우터 */
+app.use((err, req, res, next) => {
+	const data = {
+		message : err.message,
+		status : err.status || 500,
+		stack : err.stack,
+	};
+	
+	/** 로그 기록 */
+	logger(`[${data.status}]${data.message}`, 'error');
+	logger(data.stack, 'error');
+	
+	if (process.env.NODE_ENV === 'production') { // 서비스 중 
+		delete data.stack;
+	}
+	
+	return res.status(err.status || 500).render("error", data);
+});
+
+app.listen(app.get('PORT'), () => {
+	console.log(app.get('PORT'), "번 포트에서 서버 대기중...");
+});
