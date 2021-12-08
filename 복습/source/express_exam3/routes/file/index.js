261d8a216215f8335e96e2348023ed4f03f6e44f
yonggyo1981 <yonggyo1981@gmail.com>
2021-12-08 10:22:26 +0900

    .

--- 복습/source/express_exam3/routes/file/index.js
+++ 복습/source/express_exam3/routes/file/index.js
@@ -1,19 +1,54 @@
 const router = require('express').Router();
-			// /file
+const multer = require('multer');
+const path = require('path');
+
+const upload = multer({
+	storage :  multer.diskStorage({
+			destination(req, file, done) { // 파일이 저장될 디렉토리 경로
+				done(null, path.join(__dirname, "..", "..", "public/upload"));
+			},
+			filename(req, file, done) { // 파일명 규칙 
+				const uploadFileName = Date.now() + "_" + file.originalname;
+				done(null, uploadFileName);
+			}
+	}),// 파일 저장 경로
+	limits :  { fileSize :  1024 * 1024 * 10 },// 파일 용량 제한 
+	
+});
+
 router.route("/")
 		.get((req, res) => {
-			res.render("file/form");
+			//res.render("file/form");
+			//res.render("file/form2");
+			res.render("file/form3");
 		})
-		.post((req, res) => {
+		.post(upload.fields([{name : "file1"}, {name : "file2"}, {name : "file3"}]), (req, res) => {
+			console.log("업로드된 파일 정보 : ", req.files);
+			res.send("");
+		});
+		/*
+		.post(upload.array("files"), (req, res) => {
+			console.log("업로드된 파일 정보 : ", req.files);
+			res.send("");
+		});
+		*/
+		/*
+		.post(upload.single('file'), (req, res) => {
+			console.log("업로드된 파일 정보 : ", req.file);
+			/*
+			const fileData = [];
 			req.on("data", (chunk) => {
-				console.log(chunk);
+				fileData.push(chunk);
 			});
 			
 			req.on("end", () => {
-				console.log("전송 완료!");
+				console.log(fileData.toString());
 			});
+			*/
+			/*
 			res.send("");
 		});
+		*/
 	
 
 module.exports = router;
